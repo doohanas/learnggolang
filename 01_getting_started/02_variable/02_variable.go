95a62276565e6edf042c1e97cb82df1609a3f2ce
Doohan <doohanaditya@yahoo.co.id>
2022-12-26 14:47:38 +0700

    feat (variable): add multiple data

--- 01_getting_started/02_variable/02_variable.go
+++ 01_getting_started/02_variable/02_variable.go
@@ -13,8 +13,18 @@ func main () {
 	dynamicName := "Doohan" 
 	dynamicName = "Aditya"
 
+	// Multiple declaration of data
+	var (
+		firstName = "Doohan"
+		lastName = "Aditya"
+	)
+
 	fmt.Println(name) // expected: "Golang"
 	fmt.Println(typeBoolean) // expected: true
 	fmt.Println(number) // expected: 25
 	fmt.Println(dynamicName) // expected: "Aditya"
+	fmt.Println(len(name)) // expected: "6" Amount of chars
+	fmt.Println(name[0]) // expected: "byte of 'n'' / 71 "
+	fmt.Println(firstName) // expected: "Doohan"
+	fmt.Println(lastName ) // expected: "Aditya"
 }
